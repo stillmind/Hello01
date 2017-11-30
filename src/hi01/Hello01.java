6f9bb28008709e42ef8b517e42e6818d2a29b14b
stillmind <iamready@yeah.net>
2017-11-30 09:19:44 +0800

    1

--- src/hi01/Hello01.java
+++ src/hi01/Hello01.java
@@ -4,5 +4,7 @@ public class Hello01 {
 	public static void main(String[] args) {
 		System.out.println("hello 01");
 		System.out.println("hello edtion 2");
+		System.out.println("-------11-29");
+		System.out.println("2017-11-30");
 	}
 }
